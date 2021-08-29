5f1a59b8a8b0e92228e8eda75d5dae60b60e7b0b
user <kavkras83@mail.ru>
2021-08-29 20:46:42 +0300

    refactoring: spring security REST API с использованием JWT токена

---
+++ src/main/java/net/proselyte/jwtappdemo/model/Status.java
@@ -0,0 +1,10 @@
+package net.proselyte.jwtappdemo.model;
+
+/**
+ * Enumeration that represents status of domain objects - ACTIVE, DELETED, etc.
+ *
+ */
+
+public enum Status {
+    ACTIVE, NOT_ACTIVE, DELETED
+}
